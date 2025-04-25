89ccce9b4282da610fdae70ddc575c3c3b6350aa
Raynixx <94186653+x4raynixx@users.noreply.github.com>
2025-04-25 18:10:25 +0200

    Delete manual_setup directory

--- manual_setup/path.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-echo Normally do not run this File to launch a game! Instead execute command "game" in cmd!
-echo This file adds the game to the path so it can be run.
-timeout /t 99999
-python "%~dp0launcher.py" %*
